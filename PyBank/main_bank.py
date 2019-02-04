5ce7e2891c9f32206179d335f9b71f16e53e8546
John Read <read@industrialhygienist.com>
2019-02-04 14:08:49 -0800

    Solved!

---
+++ PyBank/main_bank.py
@@ -0,0 +1,96 @@
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# The average of the changes in "Profit/Losses" over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+# As an example, your analysis should look similar to the one below:
+
+#   Financial Analysis
+#   ----------------------------
+#   Total Months: 86
+#   Total: $38382578
+#   Average  Change: $-2315.12
+#   Greatest Increase in Profits: Feb-2012 ($1926159)
+#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+#import modules
+import os
+import csv
+
+#load csv
+csvpath = os.path.join("budget_data.csv")
+
+# Variables to Track
+total_months = 0
+total_PL = 0
+
+prev_PL = 0
+PL_change = 0
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999]
+
+PL_changes = []
+
+# Read File
+with open(csvpath, newline='') as PL_data:
+    csvreader = csv.DictReader(PL_data, delimiter=',')
+
+    # Loop through all the rows of data we collect
+    for row in csvreader:
+
+        # Calculate the totals
+        total_months = total_months + 1
+        total_PL = total_PL + int(row["Profit/Losses"])
+        # print(row)
+
+        # Keep track of changes
+        PL_change = int(row["Profit/Losses"]) - prev_PL
+        # print(PL_change)
+
+        # Reset the value of prev_PL to the row I completed my analysis
+        prev_PL = int(row["Profit/Losses"])
+        # print(prev_PL)
+
+        # Determine the greatest increase
+        if (PL_change > greatest_increase[1]):
+            greatest_increase[1] = PL_change
+            greatest_increase[0] = row["Date"]
+
+        if (PL_change < greatest_decrease[1]):
+            greatest_decrease[1] = PL_change
+            greatest_decrease[0] = row["Date"]
+
+        # Add to the PL_changes list
+        PL_changes.append(int(row["Profit/Losses"]))
+
+    # Set the PL average
+    PL_avg = sum(PL_changes) / len(PL_changes)
+    
+    # # Show Output
+    print()
+    print()
+    print()
+    print("Financial Analysis")
+    print("__________________________________________________________________")
+    print("Total Months: " + str(total_months))
+    print("Total PL: " + "$" + str(total_PL))
+    print("Average Change: " + "$" + str(round(sum(PL_changes) / len(PL_changes),2)))
+    print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
+    print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
+
+    # output to a text file
+
+    file = open("totals.txt","w")
+    file.write("Financial Analysis" + "\n")
+    file.write("__________________________________________________________________" + "\n")
+    file.write("Total Months: " + str(total_months) + "\n")
+    file.write("Total PL: " + "$" + str(total_PL) + "\n")
+    file.write("Average Change: " + "$" + str(round(sum(PL_changes) / len(PL_changes),2)) + "\n")
+    file.write("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + "\n")
+    file.write("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + "\n")
+    file.close()
---
+++ PyPoll/main_poll.py
@@ -0,0 +1,106 @@
+# In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
+
+# You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote.
+
+# As an example, your analysis should look similar to the one below:
+#   Election Results
+#   -------------------------
+#   Total Votes: 3521001
+#   -------------------------
+#   Khan: 63.000% (2218231)
+#   Correy: 20.000% (704200)
+#   Li: 14.000% (492940)
+#   O'Tooley: 3.000% (105630)
+#   -------------------------
+#   Winner: Khan
+#   -------------------------
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+#import modules
+import os
+import csv
+
+csvpath = os.path.join("election_data.csv")
+
+# Declare Variables. extratcted names manually as shortcut but if more time could have done this with code.
+total_votes = 0 
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
+#open csv
+with open(csvpath,newline="", encoding="utf-8") as elections:
+
+    #read csv
+    csvreader = csv.reader(elections,delimiter=",") 
+
+    #skip header
+    header = next(csvreader)     
+
+    #loop start
+    for row in csvreader: 
+
+        # count votes
+        total_votes +=1
+
+        # talley votes by canidate 
+        if row[2] == "Khan": 
+            khan_votes +=1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li": 
+            li_votes +=1
+        elif row[2] == "O'Tooley":
+            otooley_votes +=1
+
+ #lists for library creation
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+
+#create a dictionary from lists
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+#capture the summary data
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+#print the summary data in a table format
+print(f"Election Results")
+print(f"____________________________________")
+print(f"Total Votes: {total_votes}")
+print(f"____________________________________")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"____________________________________")
+print(f"Winner: {key}")
+print(f"____________________________________")
+
+#create the output file
+output_file = os.path.join("Election_Results_Summary.txt")
+
+with open(output_file,"w") as file:
+
+# Write methods to print to Elections_Results_Summary 
+    file.write(f"Election Results" + "\n")
+    file.write(f"____________________________________" + "\n")
+    file.write(f"Total Votes: {total_votes}" + "\n")
+    file.write(f"____________________________________" + "\n")
+    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})" + "\n")
+    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})" + "\n")
+    file.write(f"Li: {li_percent:.3f}% ({li_votes})" + "\n")
+    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})" + "\n")
+    file.write(f"____________________________________" + "\n")
+    file.write(f"Winner: {key}" + "\n")
+    file.write(f"____________________________________" + "\n")
